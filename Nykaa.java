4890744e0ba7fd9fadbba1d5601c9c79c164940d
Tamil020 <109712902+Tamil020@users.noreply.github.com>
2022-08-24 22:39:42 +0530

    Add files via upload

---
+++ Nykaa.java
@@ -0,0 +1,38 @@
+package project1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Nykaa {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	ChromeDriver drive=new ChromeDriver();
+	drive.get("https://www.nykaa.com/");
+    drive.manage().window().maximize();
+    drive.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    WebElement brands = drive.findElement(By.xpath("(//a[@class='css-1mavm7h'])[2]"));
+    Actions builder=new Actions(drive);
+    builder.moveToElement(brands).perform();
+    drive.findElement(By.xpath("//input[@id='brandSearchBox']")).sendKeys("L'Oreal Paris\r\n");
+    drive.findElement(By.xpath("(//a[contains(text(),'Oreal Paris')])[1]")).click();
+    String text="Buy L'Oreal Paris products online at best price on Nykaa | Nykaa";
+    String text2=drive.getTitle();
+    { 
+    	if(text.equals(text2))
+    		System.out.println("true");
+    	else
+    	{
+    		System.out.println("false");
+    		
+    	}
+    	
+    }
+}
+
+}
---
+++ SnapDeal.java
@@ -0,0 +1,24 @@
+package project1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SnapDeal {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("https://www.snapdeal.com/");
+		driver.manage().window().maximize();
+		WebElement men = driver.findElement(By.xpath("(//span[@class='catText'])[1]"));
+		Actions build=new Actions(driver);
+		build.moveToElement(men).perform();
+		driver.findElement(By.xpath("//span[@class='linkTest'][1]")).click();
+		String shoes=driver.findElement(By.xpath("//span[contains(text(),'Items')]")).getText();
+		System.out.println("shoes count:"+shoes);
+	}
+
+}
